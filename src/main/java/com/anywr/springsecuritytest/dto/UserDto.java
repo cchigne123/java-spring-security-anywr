096507f31ecc9fd3bc101202275530ccbe45104a
cchignem <cchignem@everis.com>
2023-04-24 18:47:10 -0500

    feat(login): configure user authentication details add custom user auth service and user details

--- src/main/java/com/anywr/springsecuritytest/dto/UserDto.java
+++ src/main/java/com/anywr/springsecuritytest/dto/UserDto.java
@@ -2,12 +2,15 @@ package com.anywr.springsecuritytest.dto;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.Collection;
 
 @Data
-public class UserDto {
+public class UserDto implements UserDetails {
 
     @NotBlank(message = "A username must be provided")
     @Length(max = 15, message = "Username can have up to 15 characters")
@@ -24,9 +27,33 @@ public class UserDto {
     private String lastname;
 
     @NotBlank(message = "A password must be provided")
-    @Pattern(regexp = "^[A-Za-z0-9]+$", message = "Password has to be alphanumeric. No special characters allowed")
     private String password;
 
     @Pattern(regexp = "^(.+)@(.+)$", message = "The email provided is invalid")
     private String email;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
---
+++ src/main/java/com/anywr/springsecuritytest/service/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.anywr.springsecuritytest.service;
+
+import com.anywr.springsecuritytest.domain.User;
+import com.anywr.springsecuritytest.dto.UserDto;
+import com.anywr.springsecuritytest.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optUser = this.userRepository.findUserByUsername(username);
+        if (optUser.isEmpty()) {
+            throw new UsernameNotFoundException("User " + username + " does not exist");
+        }
+        User user = optUser.get();
+        UserDto userDto = new UserDto();
+        userDto.setUsername(user.getUsername());
+        userDto.setPassword(user.getPassword());
+        userDto.setEmail(user.getEmail());
+        return userDto;
+    }
+}
