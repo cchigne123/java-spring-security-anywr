86c748fc98896c2e0bed3c42a84a4ff26fc6b7bc
cchignem <cchignem@everis.com>
2023-04-24 17:38:09 -0500

    feat: add save user db integration add repository layer and use it on service layer

--- src/main/java/com/anywr/springsecuritytest/SpringSecurityTestApplication.java
+++ src/main/java/com/anywr/springsecuritytest/SpringSecurityTestApplication.java
@@ -2,8 +2,12 @@ package com.anywr.springsecuritytest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages="com.anywr.springsecuritytest.repository")
+@EntityScan(basePackages="com.anywr.springsecuritytest.domain")
 public class SpringSecurityTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/anywr/springsecuritytest/service/UserServiceImpl.java
+++ src/main/java/com/anywr/springsecuritytest/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.anywr.springsecuritytest.service;
 
 import com.anywr.springsecuritytest.domain.User;
+import com.anywr.springsecuritytest.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +9,16 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl implements UserService {
 
     private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-    public UserServiceImpl(PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository) {
         this.passwordEncoder = passwordEncoder;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void saveUser(User user) {
         user.setPassword(this.passwordEncoder.encode(user.getPassword()));
+        this.userRepository.save(user);
     }
 }
