9411d8cae3c52f08d626c0b11e0ebec0cc291b08
cchignem <cchignem@everis.com>
2023-04-24 16:22:40 -0500

    chore: Basic project configuration

--- README.md
+++
@@ -1,1 +0,0 @@
-# java-spring-security-anywr
---
+++ src/test/java/com/anywr/springsecuritytest/SpringSecurityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anywr.springsecuritytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
