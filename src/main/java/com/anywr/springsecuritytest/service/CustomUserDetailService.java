0b916522d2fa5ab31d516067d96fa4e4dcc903cd
cchignem <cchignem@everis.com>
2023-04-24 19:46:02 -0500

    fix: user repository injection on custom user details service

--- src/main/java/com/anywr/springsecuritytest/service/CustomUserDetailService.java
+++ src/main/java/com/anywr/springsecuritytest/service/CustomUserDetailService.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class CustomUserDetailService implements UserDetailsService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
