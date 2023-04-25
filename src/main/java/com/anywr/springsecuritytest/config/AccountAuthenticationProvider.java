b9a97b53528f3ad155283d99b4634ed57851cc55
cchignem <cchignem@everis.com>
2023-04-24 19:17:17 -0500

    feat(login): sign in user-pass validation add custom authentication provider to validate password

---
+++ src/main/java/com/anywr/springsecuritytest/config/AccountAuthenticationProvider.java
@@ -0,0 +1,36 @@
+package com.anywr.springsecuritytest.config;
+
+import com.anywr.springsecuritytest.service.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class AccountAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    private final CustomUserDetailService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken token)
+            throws AuthenticationException {
+        if (token.getCredentials() == null || userDetails.getPassword() == null) {
+            throw new BadCredentialsException("Password can't be null.");
+        }
+        if (!passwordEncoder.matches((String) token.getCredentials(), userDetails.getPassword())) {
+            throw new BadCredentialsException("Invalid credentials.");
+        }
+    }
+
+    @Override
+    protected UserDetails retrieveUser(String username, UsernamePasswordAuthenticationToken token) throws AuthenticationException {
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+}
--- src/main/java/com/anywr/springsecuritytest/config/WebSecurityConfig.java
+++ src/main/java/com/anywr/springsecuritytest/config/WebSecurityConfig.java
@@ -1,26 +1,37 @@
 package com.anywr.springsecuritytest.config;
 
+import com.anywr.springsecuritytest.service.CustomUserDetailService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    private final CustomUserDetailService userDetailsService;
+    private final AccountAuthenticationProvider accountAuthenticationProvider;
+    private final AuthenticationManagerBuilder auth;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers( "/user/**").permitAll()
                 .and().csrf().disable();
+                //.authenticationManager(buildAuthenticationManagerBuilder(auth));
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.authenticationProvider(accountAuthenticationProvider);
+        return authenticationManagerBuilder.build();
     }
 
 }
